3c324a052a462fd38c69238a2661cb2b9bc0c03c
Swetha Uppula <swethauppulak@gmail.com>
2023-10-21 13:39:36 -0700

    commit after website launch

---
+++ build/static/js/496.2a0ab2d3.chunk.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:341309b75a3677e0d63a474b89af42018891fb97bc920703d3f795f025af4c1c
+size 7603
---
+++ build/static/js/main.3d78f869.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71cce3828b14b6045a14838901dee2858b580a0c1995c6ed081ab8edaeca2a8b
+size 573699
